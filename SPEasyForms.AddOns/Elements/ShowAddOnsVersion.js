5d0a4b037640465d5c3103d25112f28c787facbf
mcsheaj <joseph.mcshea@gmail.com>
2015-01-12 16:49:54 -0500

    fixed comment

--- SPEasyForms.AddOns/Elements/ShowAddOnsVersion.js
+++ SPEasyForms.AddOns/Elements/ShowAddOnsVersion.js
@@ -15,7 +15,7 @@
     // return without doing anything if SPEasyForms has not been loaded
     if (!$.spEasyForms) return;
 
-    // only operate on the 
+    // only operate on the settings page
     if (window.location.href.toLowerCase().indexOf("speasyformssettings.aspx") > -1) {
         $().ready(function () { 
             $("b:contains('Version: 2014.01')").parent().append("<br><b>AddOns: 2015.00.03</b>");
