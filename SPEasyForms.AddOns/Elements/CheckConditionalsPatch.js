da30ca16ebc892fd508d89f852f8a0b76cc08bf1
mcsheaj <joseph.mcshea@gmail.com>
2015-02-06 19:45:29 -0500

    fixed issue 18 "Partial Containers (i.e. a single tab) are not Shown/Hidden Dynamically", issue 19 "List Settings Page link to SPEasyFormsSettings Not Security Trimmed", and issue 22 "Calculated Fields not Showing on Settings Page"

--- SPEasyForms.AddOns/Elements/CheckConditionalsPatch.js
+++ SPEasyForms.AddOns/Elements/CheckConditionalsPatch.js
@@ -1,8 +1,8 @@
-﻿/*
+/*
  * SPEasyForms CheckConditionalsPatch - patch for checkConditionals to fix multiple conditionals
  * do not work (issue #10).
  *
- * @version 2015.00.06
+ * @version 2015.00.07
  * @requires SPEasyForms v2014.01 
  * @copyright 2014-2015 Joe McShea
  * @license under the MIT license:
@@ -49,148 +49,148 @@
         return result;
     };
 
-    $.spEasyForms.containerCollection.postTransform = function (options) {
+    $.spEasyForms.containerCollection.postTransform = function(options) {
         var opt = $.extend({}, $.spEasyForms.defaults, options);
-        opt.prepend = true;
+		opt.prepend = true;
         $.each(opt.currentConfig.layout.def, function (index, layout) {
-            var implementation = $.spEasyForms.utilities.jsCase(layout.containerType);
-            if (implementation in containerCollection.containerImplementations) {
-                var impl = containerCollection.containerImplementations[implementation];
-                if (typeof (impl.postTransform) === 'function') {
-                    opt.index = index;
-                    opt.currentContainerLayout = layout;
-                    opt.containerId = "spEasyFormsContainers" + (opt.prepend ? "Pre" : "Post");
-                    impl.postTransform(opt);
-                }
-            }
-            if (layout.containerType !== $.spEasyForms.defaultFormContainer.containerType) {
-                if ($("#" + opt.containerId).children().last().find("td.ms-formbody").length === 0) {
-                    $("#" + opt.containerId).children().last().hide();
-                }
-            }
-            else {
-                opt.prepend = false;
-            }
+                    var implementation = $.spEasyForms.utilities.jsCase(layout.containerType);
+                    if (implementation in containerCollection.containerImplementations) {
+                        var impl = containerCollection.containerImplementations[implementation];
+                        if (typeof (impl.postTransform) === 'function') {
+                            opt.index = index;
+                            opt.currentContainerLayout = layout;
+                            opt.containerId = "spEasyFormsContainers" + (opt.prepend ? "Pre" : "Post");
+                            impl.postTransform(opt);
+                        }
+                    }
+                    if (layout.containerType !== $.spEasyForms.defaultFormContainer.containerType) {
+                        if ($("#" + opt.containerId).children().last().find("td.ms-formbody").length === 0) {
+                            $("#" + opt.containerId).children().last().hide();
+                        }
+                    }
+                    else {
+                        opt.prepend = false;
+                    }
         });
-    };
+	};
     var containerCollection = $.spEasyForms.containerCollection;
     var baseContainer = $.spEasyForms.baseContainer;
-
-    containerCollection.containerImplementations.tabs.postTransform = function (options) {
-        var opt = $.extend({}, $.spEasyForms.defaults, options);
-        opt.divId = "spEasyFormsTabDiv" + opt.index;
-        $("#" + opt.divId + " table.speasyforms-tabs").each(function () {
-            var index = $(this)[0].id.replace("spEasyFormsTabsTable", "");
-            if ($(this).find("tr:not([data-visibilityhidden='true']) td.ms-formbody").length === 0) {
-                if ($(this).parent().css("display") !== "none") {
-                    var nextIndex = -1;
-                    if ($(this).parent().next().length > 0) {
-                        nextIndex = $(this).parent().next()[0].id.replace("spEasyFormsTabsDiv", "");
-                        $(this).parent().next().show();
-                        $("li.speasyforms-tabs" + nextIndex).addClass("ui-tabs-active").addClass("ui-state-active");
+	
+	    containerCollection.containerImplementations.tabs.postTransform = function (options) {
+            var opt = $.extend({}, $.spEasyForms.defaults, options);
+            opt.divId = "spEasyFormsTabDiv" + opt.index;
+            $("#" + opt.divId + " table.speasyforms-tabs").each(function () {
+                    var index = $(this)[0].id.replace("spEasyFormsTabsTable", "");
+                if ($(this).find("tr:not([data-visibilityhidden='true']) td.ms-formbody").length === 0) {
+                    if ($(this).parent().css("display") !== "none") {
+                        var nextIndex = -1;
+                        if ($(this).parent().next().length > 0) {
+                            nextIndex = $(this).parent().next()[0].id.replace("spEasyFormsTabsDiv", "");
+                            $(this).parent().next().show();
+                            $("li.speasyforms-tabs" + nextIndex).addClass("ui-tabs-active").addClass("ui-state-active");
+                        }
+                        $(this).parent().hide();
                     }
-                    $(this).parent().hide();
+                    $(".speasyforms-tabs" + index).hide();
                 }
-                $(".speasyforms-tabs" + index).hide();
-            }
-            else {
-                $(".speasyforms-tabs" + index).show();
-            }
-        });
-    };
+				else {
+					$(".speasyforms-tabs" + index).show();
+				}
+            });
+        };
 
-    containerCollection.containerImplementations.accordion.postTransform = function (options) {
-        var opt = $.extend({}, $.spEasyForms.defaults, options);
-        var divId = "spEasyFormsAccordionDiv" + opt.index;
-        $("#" + divId + " table.speasyforms-accordion").each(function () {
-            var index = $(this)[0].id.replace("spEasyFormsAccordionTable", "");
-            if ($(this).find("tr:not([data-visibilityhidden='true']) td.ms-formbody").length === 0) {
-                $("#spEasyFormsAccordionHeader" + index).hide();
-                $("#spEasyFormsAccordionHeader" + index).next().hide();
-            }
-            else {
-                $("#spEasyFormsAccordionHeader" + index).show();
-            }
-        });
-    };
+        containerCollection.containerImplementations.accordion.postTransform = function (options) {
+            var opt = $.extend({}, $.spEasyForms.defaults, options);
+            var divId = "spEasyFormsAccordionDiv" + opt.index;
+            $("#" + divId + " table.speasyforms-accordion").each(function () {
+                    var index = $(this)[0].id.replace("spEasyFormsAccordionTable", "");
+                if ($(this).find("tr:not([data-visibilityhidden='true']) td.ms-formbody").length === 0) {
+                    $("#spEasyFormsAccordionHeader" + index).hide();
+                    $("#spEasyFormsAccordionHeader" + index).next().hide();
+                }
+				else {
+                    $("#spEasyFormsAccordionHeader" + index).show();					 
+				}
+            });
+        };
 
-    containerCollection.containerImplementations.columns.postTransform = function (options) {
-        var opt = $.extend({}, $.spEasyForms.defaults, options);
-        var outerTableId = "spEasyFormsColumnsOuterTable" + opt.index;
-        $("#" + outerTableId + " tr.speasyforms-columnrow").each(function () {
-            if ($(this).find("tr:not([data-visibilityhidden='true']) td.ms-formbody").length === 0) {
-                $(this).hide();
-            }
-            else {
-                $(this).show();
-            }
-        });
-    };
-
-    $.spEasyForms.visibilityRuleCollection.transform = function (options) {
-        var opt = $.extend({}, $.spEasyForms.defaults, options);
-        if (opt.currentConfig && opt.currentConfig.visibility && opt.currentConfig.visibility.def &&
-            Object.keys(opt.currentConfig.visibility.def).length > 0) {
-            $.each($.spEasyForms.containerCollection.rows, function (idx, row) {
-                opt.row = row;
-                if (row.internalName in opt.currentConfig.visibility.def) {
-                    var ruleHandled = false;
-                    $.each(opt.currentConfig.visibility.def[row.internalName], function (index, rule) {
-                        opt.rule = rule;
-                        if (!ruleHandled) {
-                            var formMatch = visibilityRuleCollection.checkForm(opt);
-                            var appliesMatch = visibilityRuleCollection.checkAppliesTo(opt);
-                            var conditionalMatch = visibilityRuleCollection.checkConditionals(opt);
-                            if (formMatch && appliesMatch && conditionalMatch) {
-                                var stateHandler = $.spEasyForms.utilities.jsCase(rule.state);
-                                if (stateHandler in visibilityRuleCollection.stateHandlers) {
-                                    visibilityRuleCollection.scrubCollection(opt.row.row);
-                                    visibilityRuleCollection.stateHandlers[stateHandler](opt);
-                                    ruleHandled = true;
+		containerCollection.containerImplementations.columns.postTransform = function (options) {
+			var opt = $.extend({}, $.spEasyForms.defaults, options);
+			var outerTableId = "spEasyFormsColumnsOuterTable" + opt.index;
+			$("#" + outerTableId + " tr.speasyforms-columnrow").each(function () {
+				if ($(this).find("tr:not([data-visibilityhidden='true']) td.ms-formbody").length === 0) {
+					$(this).hide();
+				}
+				else {
+					$(this).show();				
+				}
+			});
+		};
+	
+        $.spEasyForms.visibilityRuleCollection.transform = function (options) {
+            var opt = $.extend({}, $.spEasyForms.defaults, options);
+            if (opt.currentConfig && opt.currentConfig.visibility && opt.currentConfig.visibility.def &&
+                Object.keys(opt.currentConfig.visibility.def).length > 0) {
+                $.each($.spEasyForms.containerCollection.rows, function (idx, row) {
+                    opt.row = row;
+                    if (row.internalName in opt.currentConfig.visibility.def) {
+                        var ruleHandled = false;
+                        $.each(opt.currentConfig.visibility.def[row.internalName], function (index, rule) {
+                            opt.rule = rule;
+                            if (!ruleHandled) {
+                                var formMatch = visibilityRuleCollection.checkForm(opt);
+                                var appliesMatch = visibilityRuleCollection.checkAppliesTo(opt);
+                                var conditionalMatch = visibilityRuleCollection.checkConditionals(opt);
+                                if (formMatch && appliesMatch && conditionalMatch) {
+                                    var stateHandler = $.spEasyForms.utilities.jsCase(rule.state);
+                                    if (stateHandler in visibilityRuleCollection.stateHandlers) {
+                                        visibilityRuleCollection.scrubCollection(opt.row.row);
+                                        visibilityRuleCollection.stateHandlers[stateHandler](opt);
+                                        ruleHandled = true;
+                                    }
                                 }
                             }
+                            if (rule.conditions) {
+                                $.each(rule.conditions, function (idx, condition) {
+                                    var tr = $.spEasyForms.containerCollection.rows[condition.name];
+                                    if (tr === undefined) {
+                                        tr = {
+                                            displayName: condition.name,
+                                            internalName: condition.name,
+                                            spFieldType: condition.name,
+                                            value: "",
+                                            row: $("<tr><td class='ms-formlabel'><h3 class='ms-standardheader'></h3></td><td class='ms-formbody'></td></tr>"),
+                                            fieldMissing: true
+                                        };
+                                    }
+                                    if (!tr.fieldMissing && tr.row.attr("data-visibilitychangelistener") !== "true") {
+                                        tr.row.find("input").change(function () {
+                                            visibilityRuleCollection.transform(opt);
+                                            $.spEasyForms.adapterCollection.transform(opt);
+                                            $.spEasyForms.containerCollection.postTransform(opt);
+                                        });
+                                        tr.row.find("select").change(function () {
+                                            visibilityRuleCollection.transform(opt);
+                                            $.spEasyForms.adapterCollection.transform(opt);
+                                            $.spEasyForms.containerCollection.postTransform(opt);
+                                        });
+                                        tr.row.find("textarea").change(function () {
+                                            visibilityRuleCollection.transform(opt);
+                                            $.spEasyForms.adapterCollection.transform(opt);
+                                            $.spEasyForms.containerCollection.postTransform(opt);
+                                        });
+                                        tr.row.attr("data-visibilitychangelistener", "true");
+                                    }
+                                });
+                            }
+                        });
+                        if (!ruleHandled) {
+                            visibilityRuleCollection.scrubCollection(opt.row.row);
                         }
-                        if (rule.conditions) {
-                            $.each(rule.conditions, function (idx, condition) {
-                                var tr = $.spEasyForms.containerCollection.rows[condition.name];
-                                if (tr === undefined) {
-                                    tr = {
-                                        displayName: condition.name,
-                                        internalName: condition.name,
-                                        spFieldType: condition.name,
-                                        value: "",
-                                        row: $("<tr><td class='ms-formlabel'><h3 class='ms-standardheader'></h3></td><td class='ms-formbody'></td></tr>"),
-                                        fieldMissing: true
-                                    };
-                                }
-                                if (!tr.fieldMissing && tr.row.attr("data-visibilitychangelistener") !== "true") {
-                                    tr.row.find("input").change(function () {
-                                        visibilityRuleCollection.transform(opt);
-                                        $.spEasyForms.adapterCollection.transform(opt);
-                                        $.spEasyForms.containerCollection.postTransform(opt);
-                                    });
-                                    tr.row.find("select").change(function () {
-                                        visibilityRuleCollection.transform(opt);
-                                        $.spEasyForms.adapterCollection.transform(opt);
-                                        $.spEasyForms.containerCollection.postTransform(opt);
-                                    });
-                                    tr.row.find("textarea").change(function () {
-                                        visibilityRuleCollection.transform(opt);
-                                        $.spEasyForms.adapterCollection.transform(opt);
-                                        $.spEasyForms.containerCollection.postTransform(opt);
-                                    });
-                                    tr.row.attr("data-visibilitychangelistener", "true");
-                                }
-                            });
-                        }
-                    });
-                    if (!ruleHandled) {
-                        visibilityRuleCollection.scrubCollection(opt.row.row);
                     }
-                }
-            });
-        }
-    };
+                });
+            }
+        };
     var visibilityRuleCollection = $.spEasyForms.visibilityRuleCollection;
 
 })(typeof (spefjQuery) === 'undefined' ? null : spefjQuery);
