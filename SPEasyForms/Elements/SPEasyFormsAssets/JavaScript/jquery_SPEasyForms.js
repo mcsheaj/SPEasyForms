06293e4518fdddc9df3112edff5f2e22641e0c83
mcsheaj <joseph.mcshea@gmail.com>
2016-01-02 14:21:57 -0500

    fixed bug of SP.UI.ModalDialog.get_childDialog function not existing while in the editor, causing the editor to hang when you switch tabs/pages

--- SPEasyForms/Elements/SPEasyFormsAssets/JavaScript/jquery_SPEasyForms.js
+++ SPEasyForms/Elements/SPEasyFormsAssets/JavaScript/jquery_SPEasyForms.js
@@ -33690,21 +33690,23 @@ function shouldSPEasyFormsRibbonButtonBeEnabled() {
         },
 
         resizeModalDialog: function () {
-            var dlg = SP.UI.ModalDialog.get_childDialog();
-            if (dlg !== null) {
-                SP.UI.ModalDialog.get_childDialog().autoSize();
-                var dlgContent = $(".ms-dlgContent", window.parent.document);
-                var top = ($(window.top).height() - dlgContent.outerHeight()) / 2;
-                var left = ($(window.top).width() - dlgContent.outerWidth()) / 2;
-                dlgContent.css({ top: (top > 0 ? top : 0), left: (left > 0 ? left : 0) });
-                dlgContent.prev().css({ top: (top > 0 ? top : 0), left: (left > 0 ? left : 0) });
+            if (typeof (SP.UI.ModalDialog.get_childDialog) === "function") {
+                var dlg = SP.UI.ModalDialog.get_childDialog();
+                if (dlg !== null) {
+                    SP.UI.ModalDialog.get_childDialog().autoSize();
+                    var dlgContent = $(".ms-dlgContent", window.parent.document);
+                    var top = ($(window.top).height() - dlgContent.outerHeight()) / 2;
+                    var left = ($(window.top).width() - dlgContent.outerWidth()) / 2;
+                    dlgContent.css({ top: (top > 0 ? top : 0), left: (left > 0 ? left : 0) });
+                    dlgContent.prev().css({ top: (top > 0 ? top : 0), left: (left > 0 ? left : 0) });
 
-                var dlgFrame = $(".ms-dlgFrame", window.parent.document);
-                if (dlgFrame.height() > $(window.parent).height()) {
-                    dlgFrame.height($(window.parent).height());
-                }
-                if (dlgFrame.width() > $(window.parent).width()) {
-                    dlgFrame.width($(window.parent).width());
+                    var dlgFrame = $(".ms-dlgFrame", window.parent.document);
+                    if (dlgFrame.height() > $(window.parent).height()) {
+                        dlgFrame.height($(window.parent).height());
+                    }
+                    if (dlgFrame.width() > $(window.parent).width()) {
+                        dlgFrame.width($(window.parent).width());
+                    }
                 }
             }
         }
